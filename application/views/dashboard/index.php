085a89b29f48d502afdfdb4ad14ab4db488d5dbd
DikyArif_ <rahmandiky000@gmail.com>
2021-06-23 13:55:26 +0700

    baru

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,90 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <!doctype html>
+<html lang="en">
+    <body>
+    <link rel="stylesheet" type="text/css" href="assets/css/dashboard.css">
+        <section id="home" class="py-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-5">
+                         <p>Anda bingung mencari ekspedisi pengantaran barang terpercaya?. 
+                            <br>
+                        #KAMISOLUSINYA</p>
+                    </div>                    
+                    <div class="col-sm-7">
+                        <img src="assets/img/kapal.jpg" height="350px" width="auto">
+                        </div>
+                    </div>  
+                </div>
+        </section>
+        <section id="footer">
+        <footer>
+            <div class="main-content">
+                <div class="left box">
+                    <h2>  </h2>
+                    <hr>
+                    <div class="content">
+                        <p>MyCargo adalah sebuah laporan pendataan kapal ekspedisi saat kapal berlabuh atau meninggalkan pelabuhan. Jadi MyCargo adalah aplikasinya pengiriman barang berbasis web, yang mengutamakan barang berbobot ringan hingga berat sekali pun. MyCargo juga dapat menjamin pengirim barang kurang lebih dari 7 hari dan barang tetap dengan keadaan baik.</p>
+                    </div>
+                </div>
+            <div class="center box">
+                <h2> address </h2>
+                <hr>
+                <div class="content">
+                    <div class="place">
+                        <i class="fas fa-map-marker-alt"></i>
+                        <span class="text"> Semarang, Jawa Tengah</span>
+                    </div>
+                    <div class="phone">
+                        <i class="fas fa-phone-alt"></i>
+                        <span class="text"> +62 82250707070</span>
+                    </div>
+                    <div class="email">
+                        <i class="fas fa-envelope"></i>
+                        <span class="text"> mycargo@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+                <div class="right box">
+                    <h2> contact us </h2>
+                    <hr>        
+                    <div class="content">
+                        <form action="#">
+                            <div class="email">
+                                <div class="text"> Email *</div>
+                                <input type="email" required="">
+                            </div>
+                            <div class="message">
+                                <div class="text"> Message *</div>
+                                <textarea rows="2" cols="25" required=""></textarea>
+                            </div>
+                            <div class="btn">
+                                <button type="submit"> Send </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>            
+        </footer>
+     </section> 
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
+
+
+
+
+
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
